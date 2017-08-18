50c9ceea12b0c2581856921baaac93a87f5eb4e2
bhargav <bhargav.r.raut@gmail.com>
2017-08-19 04:04:36 +0530

    reads file into es, write tokenizers and analyzers in mappings next

--- app/models/mesh.rb
+++ app/models/mesh.rb
@@ -1,3 +1,41 @@
+require 'elasticsearch/persistence/model'
 class Mesh
 	include Elasticsearch::Persistence::Model
+	
+	before_save{
+		arr = []
+		self.numbers.map { |e|
+			e.scan(/\./) { |match|
+				jj = Regexp.last_match
+				arr << jj.pre_match
+			}
+		}
+		self.parents = arr.uniq.reject { |e|
+			self.numbers.include? e
+		}
+	}
+
+	##MESH "UI"
+	attribute :ui, String
+
+	attribute :parents, String, default: []
+
+	##MESH "MH"
+	attribute :name, String
+	
+	##MESH "MN" -> list of numbers
+	attribute :numbers, String, default:[]
+	
+
+	##MESH "MS"
+	attribute :description, String
+
+	##MESH "AN"
+	attribute :annotation, String
+
+	##MESH "ENTRY"
+	attribute :other_names, String, default:[]
+
+
+
 end
