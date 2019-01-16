f2d4812028b08908c0711ba6b33072574d6c1d47
bhargav raut <bhargav.r.raut@gmail.com>
2019-01-16 23:31:10 +0530

    updated readme

--- config/initializers/es.rb
+++ config/initializers/es.rb
@@ -1,5 +1,5 @@
-remote_host = {host: '8c8c4a8f1c5d41cda38b517c2407070b.us-east-1.aws.found.io', scheme: 'https', port: 9243}
-remote_host.merge!({user: 'elastic', password: 'fM5mTFWHGkp2TfceF0dXgVz4'})
+remote_host = {host: ENV["REMOTE_ES_HOST"] , scheme: 'https', port: 9243}
+remote_host.merge!({user: ENV["REMOTE_ES_USER"], password: ENV["REMOTE_ES_PASSWORD"]})
 
 $remote_es_client = Elasticsearch::Client.new hosts: [ remote_host], headers: {"Content-Type" => "application/json" }, request: { timeout: 45 }
 
